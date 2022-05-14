0d9e2a8eaf6162f0a121c6f8de5e6b8380777c00
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-05-14 10:47:11 +0700

    small fix

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 
 const app = require('./app');
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port ', process.env.PORT);
 });
